01f2dc009beeaab2f080a497b8d995303079a971
samuel-blankson <72501108+samuel-blankson@users.noreply.github.com>
2022-07-28 06:28:37 +0000

    running app

--- Dockerfile
+++ Dockerfile
@@ -21,5 +21,5 @@ EXPOSE 80
 
 ## Step 5:
 # Run app.py at container launch
-CMD ["python", "-m","flask","run", "--host=0.0.0.0", "--port=8000"]
+CMD ["python", "app.py"]
 
--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=microsersive-project .
+docker build --tag=machine-learning-api .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run --publish 8000:8000 microsersive-project
+docker run -p 8000:80 machine-learning-api
