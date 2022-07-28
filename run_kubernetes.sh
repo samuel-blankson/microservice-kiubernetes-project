19cf2945e47ec8998a1c326515c1e2984f5a0a07
samuel-blankson <72501108+samuel-blankson@users.noreply.github.com>
2022-07-28 21:46:30 +0000

    run with kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=fiifigemini/machine-learning-api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run machine-learning-api --image=$dockerpath --port=8000
 
 # Step 3:
-# List kubernetes pods
+# List kubernetes pods 
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward machine-learning-api 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-dockerpath=fiifigemini/machine-learning-api
+dockerpath=/machine-learning-api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+aws ecr get-login-password --region us-east-1
+docker tag machine-learning-api:latest 402291212186.dkr.ecr.us-east-1.amazonaws.com/machine-learning-api:latest
 
 # Step 3:
 # Push image to a docker repository 
+docker push 402291212186.dkr.ecr.us-east-1.amazonaws.com/machine-learning-api:latest
+
