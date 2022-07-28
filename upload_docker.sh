0dbd55d50de57873bc1a490607d7e8b567ebfd6f
samuel-blankson <72501108+samuel-blankson@users.noreply.github.com>
2022-07-28 15:11:39 +0000

    docker output

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,11 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=fiifigemini/machine-learning-api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
-# Push image to a docker repository
+# Push image to a docker repository 
